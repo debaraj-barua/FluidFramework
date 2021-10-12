cf492c8a67d103785e9339a4d2441967534f7e75
Tyler Butler <tylerbu@microsoft.com>
2021-10-12 14:27:10 -0700

    cookie to show/hide update banner

--- docs/themes/thxvscode/assets/js/jquery/updates.js
+++ docs/themes/thxvscode/assets/js/jquery/updates.js
@@ -1,12 +1,12 @@
 export function loadUpdatesBanner() {
   if (document.cookie.indexOf('hide-update-notification=') === -1) {
-    $('.updates-banner').removeClass('invisible');
+    $('.updates-banner').removeClass('hidden');
   }
 
   $('.updates-banner div.dismiss-btn').on('click', function() {
     Cookies.set('hide-update-notification', 'true', {
-      expires: 25 // hide until the next update is live
+      expires: 14 // hide until the next update is live
     });
-    $('.updates-banner').addClass('invisible');
+    $('.updates-banner').addClass('hidden');
   })
 }

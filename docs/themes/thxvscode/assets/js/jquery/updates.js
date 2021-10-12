bcc22f065d3e066836af36a647cbd8a9f8d4fdd5
Tyler Butler <tylerbu@microsoft.com>
2021-10-12 12:30:03 -0700

    wip

--- docs/themes/thxvscode/assets/js/jquery/updates.js
+++ docs/themes/thxvscode/assets/js/jquery/updates.js
@@ -1,4 +1,8 @@
 export function loadUpdatesBanner() {
+  if (document.cookie.indexOf('hide-update-notification=') === -1) {
+    $('.updates-banner').removeClass('invisible');
+  }
+
   $('.updates-banner div.dismiss-btn').on('click', function() {
     Cookies.set('hide-update-notification', 'true', {
       expires: 25 // hide until the next update is live
